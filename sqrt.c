c8e2c86a60079b659bac01e91c1a17ac5b00fb7d
yogeshdeverth155 <57284848+yogeshdeverth155@users.noreply.github.com>
2020-10-16 16:49:21 +0530

    NEGSUPPORT added

--- sqrt.c
+++ sqrt.c
@@ -9,13 +9,13 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT:");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("End of program. Exiting");
+	printf("NEGSUPPORT:");
+	printf("NEGSUPPORT:");
 	return(0);
 
 } // end main
